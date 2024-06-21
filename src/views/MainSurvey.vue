ece6a477fdeb28e8e7bbedaba5a5fa6328c12024
Ruslan <ruslansharipov19980111@gmail.com>
2024-06-21 10:14:31 +0500

    refactor(MainSurvey): reduce CSS selector nesting to three levels

--- src/views/MainSurvey.vue
+++ src/views/MainSurvey.vue
@@ -1,7 +1,6 @@
 <template>
   <Header />
   <div class="main-survey-content">
-    <!-- <div class="main-survey-content__img"> -->
     <div class="main-survey-content__image-container">
       <img
         class="main-survey-content__image"
@@ -83,103 +82,102 @@ const selectRating = (rating) => {
 .main-survey-content {
   margin: 0 auto;
   max-width: 1052px;
-  /* height: 468px; */
   width: 84%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+}
 
-  &__image-container {
+.main-survey-content__image-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 12px 0px;
+}
+
+.main-survey-content__image {
+  max-width: 456px;
+  height: auto;
+}
+
+.main-survey-content__form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 552px;
+  padding: 10px;
+  box-sizing: border-box;
+  gap: 28px;
+
+  .main-survey-content__form-header {
     width: 100%;
-    display: flex;
-    justify-content: center;
-    padding: 12px 0px;
-  }
-  &__image {
-    max-width: 456px;
-    height: auto;
-  }
-
-  &__form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 552px;
-    padding: 10px;
-
-    box-sizing: border-box;
-    gap: 28px;
-
-    &-header {
-      width: 100%;
-      font-family: Roboto;
-      font-size: 30px;
-      font-weight: 700;
-      line-height: 35px;
-      text-align: center;
-      color: white;
-    }
-
-    &-subheader {
-      width: 100%;
-      font-family: Roboto;
-      font-size: 17px;
-      font-weight: 400;
-      line-height: 22px;
-      text-align: center;
-      color: white;
-    }
+    font-family: Roboto;
+    font-size: 30px;
+    font-weight: 700;
+    line-height: 35px;
+    text-align: center;
+    color: white;
   }
 
-  &__rating {
+  .main-survey-content__form-subheader {
     width: 100%;
-    display: flex;
-    flex-direction: column;
-    gap: 8px;
-
-    &-buttons {
-      width: 100%;
-      display: flex;
-      /* align-items: center; */
-      justify-content: space-between;
-      /* gap: 10px; */
-      flex-direction: row;
-      flex-wrap: wrap;
-
-      .rating-button {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        border: 1px solid rgba(31, 115, 249, 1);
-        color: white;
-        font-family: Roboto, sans-serif;
-        font-size: 14px;
-        font-weight: 500;
-        cursor: pointer;
-      }
-
-      &-label {
-        font-family: Roboto, sans-serif;
-        font-size: 14px;
-        font-weight: 400;
-      }
-    }
-    &-captions {
-      width: 100%;
-      display: flex;
-      justify-content: space-between;
-      font-family: Roboto;
-      font-size: 14px;
-      font-weight: 400;
-      line-height: 20px;
-      text-align: left;
-      color: #bfc9d4;
-    }
+    font-family: Roboto;
+    font-size: 17px;
+    font-weight: 400;
+    line-height: 22px;
+    text-align: center;
+    color: white;
   }
+}
+
+.main-survey-content__rating {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+.main-survey-content__rating-buttons {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  /* gap: 10px; */
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.rating-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  border: 1px solid rgba(31, 115, 249, 1);
+  color: white;
+  font-family: Roboto, sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+}
+
+// &-label {
+//   font-family: Roboto, sans-serif;
+//   font-size: 14px;
+//   font-weight: 400;
+// }
+
+.main-survey-content__rating-captions {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  font-family: Roboto;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  text-align: left;
+  color: #bfc9d4;
 }
 
 /* tablet*/
@@ -211,73 +209,72 @@ const selectRating = (rating) => {
 @media screen and (max-width: 376px) {
   .main-survey-content {
     width: 100%;
+  }
 
-    &__image-container {
-      width: 100%;
+  .main-survey-content__image-container {
+    width: 100%;
 
-      height: auto;
-    }
-    &__image {
-      width: 100%;
-    }
+    height: auto;
+  }
 
-    &__form {
-      width: 100%;
-      /* height: 280px; */
-      padding: 10px 16px 10px 16px;
-      gap: 14px;
+  .main-survey-content__image {
+    width: 100%;
+  }
 
-      display: flex;
-      flex-direction: column;
-      align-items: center;
+  .main-survey-content__form {
+    width: 100%;
+    padding: 10px 16px 10px 16px;
+    gap: 14px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-      &-header {
-        font-size: 22px;
-        font-weight: 700;
-        line-height: 35px;
-        text-align: center;
-      }
-
-      &-subheader {
-        font-family: Roboto;
-        font-size: 12px;
-        line-height: 20px;
-        text-align: center;
-      }
+    &-header {
+      font-size: 22px;
+      font-weight: 700;
+      line-height: 35px;
+      text-align: center;
     }
 
-    &__rating {
-      align-items: center;
-      height: Hug (118px) px;
-
-      &-buttons {
-        width: 100%;
-        height: 90px;
-        padding: 0px 60px 0px 60px;
-        justify-content: space-between;
-        box-sizing: border-box;
-      }
-
-      &-circles {
-        padding: 0px 60px 0px 60px;
-        box-sizing: border-box;
-
-        &-label {
-          font-family: Roboto;
-          line-height: 20px;
-          text-align: center;
-          font-family: Roboto, sans-serif;
-          font-size: 14px;
-          font-weight: 400;
-        }
-      }
-      .caption {
-        /* width: 343px;*/
-        height: 20px;
-        padding: 0px 32px 0px 32px;
-        box-sizing: border-box;
-      }
+    &-subheader {
+      font-family: Roboto;
+      font-size: 12px;
+      line-height: 20px;
+      text-align: center;
     }
+  }
+
+  .main-survey-content__rating {
+    align-items: center;
+    height: Hug (118px) px;
+  }
+
+  .main-survey-content__rating-buttons {
+    width: 100%;
+    height: 90px;
+    padding: 0px 60px 0px 60px;
+    justify-content: space-between;
+    box-sizing: border-box;
+  }
+
+  .main-survey-content__rating-circles {
+    padding: 0px 60px 0px 60px;
+    box-sizing: border-box;
+
+    &-label {
+      font-family: Roboto;
+      line-height: 20px;
+      text-align: center;
+      font-family: Roboto, sans-serif;
+      font-size: 14px;
+      font-weight: 400;
+    }
+  }
+
+  .main-survey-content__caption {
+    height: 20px;
+    padding: 0px 32px 0px 32px;
+    box-sizing: border-box;
   }
 }
 </style>
